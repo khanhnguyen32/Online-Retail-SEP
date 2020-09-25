dcc9f59e0cadbbabb8b4b16ce151c6796ae4bab2
Nguyễn Quốc Khánh <47747687+khanhnguyen32@users.noreply.github.com>
2020-09-25 18:53:05 +0700

    commit to 'master'

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ let app = express();
 //Set Public Static Folder
 app.use(express.static(__dirname + '/public'));
 
+//Use View Engine
+let expressHbs = require('express-handlebars');
+let hbs = expressHbs.create({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + "/view/layouts",
+    partialsDir: __dirname + '/views/partials/'
+});
+
 //Set Server Port & Start Server
 app.set('port',process.env.PORT || 5000);
 app.listen(app.get('port'), () =>{
