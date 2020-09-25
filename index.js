fc4a5e44910681114d9604852fb91fdb7bdcba2e
Nguyễn Quốc Khánh <47747687+khanhnguyen32@users.noreply.github.com>
2020-09-25 18:49:34 +0700

    commit to 'master'

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let app = express();
 
+//Set Public Static Folder
+app.use(express.static(__dirname + '/public'));
+
 //Set Server Port & Start Server
 app.set('port',process.env.PORT || 5000);
 app.listen(app.get('port'), () =>{
