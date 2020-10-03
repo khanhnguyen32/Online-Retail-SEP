fbaf6d6ff69c9a5606dfe84fc9ec809ebcb30930
Nguyễn Quốc Khánh <47747687+khanhnguyen32@users.noreply.github.com>
2020-10-03 20:55:11 +0700

    cho gọn

--- models/user.js
+++ models/user.js
@@ -13,8 +13,6 @@ module.exports = (sequelize, DataTypes) => {
             // define association here
             User.hasMany(models.Comment, { foreignKey: 'userId' });
             User.hasMany(models.Review, { foreignKey: 'userId' });
-
-
         }
     };
     User.init({
