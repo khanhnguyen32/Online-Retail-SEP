54df80facfaf468a720a431ab7fcb646d5a10bca
sonicthinh2000 <58232543+sonicthinh2000@users.noreply.github.com>
2020-10-05 01:15:00 +0700

    brand and color

---
+++ seeders/02-demo-brand.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        let data = [
+            { name: "Must have", imagepath: 'img/home/item1.jpg' },
+            { name: "H&M", imagepath: 'img/home/item1.jpg' },
+            { name: "Unique", imagepath: 'img/home/item1.jpg' },
+            { name: "Adidas", imagepath: 'img/home/item1.jpg' },
+            { name: "Zara", imagepath: 'img/home/item1.jpg' },
+            { name: "Louis Vuitton", imagepath: 'img/home/item1.jpg' },
+            { name: "Gucci", imagepath: 'img/home/item1.jpg' },
+            { name: "Chanel", imagepath: 'img/home/item1.jpg' },
+        ];
+        data.map(item => {
+            item.createdAt = Sequelize.literal('NOW()');
+            item.updatedAt = Sequelize.literal('NOW()');
+        });
+        await queryInterface.bulkInsert('Brands', data, {});
+    },
+
+    down: async(queryInterface, Sequelize) => {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+    }
+};
