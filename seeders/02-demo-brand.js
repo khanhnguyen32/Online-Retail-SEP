d2d332e482c9d618d6d6f3cb48a743e42dbeca2d
Nguyễn Quốc Khánh <47747687+khanhnguyen32@users.noreply.github.com>
2020-10-05 14:10:24 +0700

    edit name

--- seeders/02-demo-brand.js
+++ seeders/02-demo-brand.js
@@ -3,7 +3,6 @@
 module.exports = {
     up: async(queryInterface, Sequelize) => {
         let data = [
-            { name: "Must have", imagepath: 'img/home/item1.jpg' },
             { name: "H&M", imagepath: 'img/home/item1.jpg' },
             { name: "Unique", imagepath: 'img/home/item1.jpg' },
             { name: "Adidas", imagepath: 'img/home/item1.jpg' },
--- seeders/03-demo-color.js
+++ seeders/03-demo-color.js
@@ -6,31 +6,31 @@ module.exports = {
             "name": "Crimson",
             "code": "#0e6d72",
             "imagepath": "http://dummyimage.com/30x30.jpg/ff4444/ffffff"
-          }, {
+        }, {
             "name": "Indigo",
             "code": "#ca61d1",
             "imagepath": "http://dummyimage.com/30x30.bmp/cc0000/ffffff"
-          }, {
+        }, {
             "name": "Purple",
             "code": "#2fe06d",
             "imagepath": "http://dummyimage.com/30x30.png/ff4444/ffffff"
-          }, {
+        }, {
             "name": "Puce",
             "code": "#4fd064",
             "imagepath": "http://dummyimage.com/30x30.jpg/5fa2dd/ffffff"
-          }, {
+        }, {
             "name": "Blue",
             "code": "#b6cbfd",
             "imagepath": "http://dummyimage.com/30x30.bmp/ff4444/ffffff"
-          }, {
+        }, {
             "name": "Puce",
             "code": "#0059d4",
             "imagepath": "http://dummyimage.com/30x30.png/dddddd/000000"
-          }, {
-            "name": "Indigo",
+        }, {
+            "name": "Mamnem",
             "code": "#896465",
             "imagepath": "http://dummyimage.com/30x30.jpg/dddddd/000000"
-          }];
+        }];
         data.map(item => {
             item.createdAt = Sequelize.literal('NOW()');
             item.updatedAt = Sequelize.literal('NOW()');
