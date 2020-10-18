fb7d202dfa375bea573de8a0ed82caf83399c4d0
Nguyễn Quốc Khánh <47747687+khanhnguyen32@users.noreply.github.com>
2020-10-18 20:58:20 +0700

    test

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,15 +1,15 @@
-let controller = {};
-let models = require('../models')
-let Category = models.Category;
+// let controller = {};
+// let models = require('../models')
+// let Category = models.Category;
 
-controller.getAll = () => {
-    return new Promise((resolve, reject) => {
-        Category
-            .findAll({
-                attributes: ['id', 'name', 'imagepath', 'summary']
-            })
-            .then(data => resolve(data))
-            .catch(error => reject(new Error(error)));
-    });
-};
-module.exports = controller;
+// controller.getAll = () => {
+//     return new Promise((resolve, reject) => {
+//         Category
+//             .findAll({
+//                 attributes: ['id', 'name', 'imagepath', 'summary']
+//             })
+//             .then(data => resolve(data))
+//             .catch(error => reject(new Error(error)));
+//     });
+// };
+// module.exports = controller;
