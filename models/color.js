b19ac703111e7de947d122fcbf40e6371a9c9220
Nguyễn Quốc Khánh <47747687+khanhnguyen32@users.noreply.github.com>
2020-10-18 18:25:06 +0700

    fix

--- models/color.js
+++ models/color.js
@@ -1,28 +1,28 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Color extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      Color.hasMany(models.ProductColor, {foreignKey: 'colorId'});
+    class Color extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            Color.hasMany(models.ProductColor, { foreignKey: 'colorId' });
 
-    }
-  };
-  Color.init({
-    name: DataTypes.STRING,
-    code: DataTypes.STRING,
-    sumary: DataTypes.TEXT,
-    imagepath: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Color',
-  });
-  return Color;
+        }
+    };
+    Color.init({
+        name: DataTypes.STRING,
+        code: DataTypes.STRING,
+        summary: DataTypes.TEXT,
+        imagepath: DataTypes.TEXT
+    }, {
+        sequelize,
+        modelName: 'Color',
+    });
+    return Color;
 };
