25a197f0fa33779178436f54138dd2ed32b54ab9
Nguyen Quoc Khanh <khanhng.32@gmail.com>
2020-12-09 20:58:14 +0700

    done cart

--- public/js/project.js
+++ public/js/project.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
 
 function addToCart() {
     var id = $(this).data('id');
-    var quantity = 1;
+    var quantity = $('#sst') ? $('#sst').val() : 1;
     $.ajax({
         url: '/cart',
         type: 'POST',
@@ -12,5 +12,56 @@ function addToCart() {
         success: function(result) {
             $('#cart-badge').html(result.totalQuantity);
         }
-    })
+    });
+}
+
+function updateCart(id, quantity) {
+    if (quantity == 0) {
+        removeCartItem(id);
+    } else {
+        updateCartItem(id, quantity);
+    }
+}
+
+function removeCartItem(id) {
+    $.ajax({
+        url: '/cart',
+        type: 'DELETE',
+        data: { id },
+        success: function(result) {
+            $('#cart-badge').html(result.totalQuantity);
+            $('#totalPrice').html('VND' + result.totalPrice);
+            if (result.totalQuantity > 0) {
+                $(`#item${id}`).remove();
+            } else {
+                $('#cart-body').html(`<div class="alert alert-info text-center"> Your cart is empty!</div>`);
+            }
+        }
+    });
+}
+
+function updateCartItem(id, quantity) {
+    $.ajax({
+        url: '/cart',
+        type: 'PUT',
+        data: { id, quantity },
+        success: function(result) {
+            $('#cart-badge').html(result.totalQuantity);
+            $('#totalPrice').html('VND' + result.totalPrice);
+            $(`#price${id}`).html('VND' + result.item.price);
+        }
+    });
+}
+
+function clearCart() {
+    if (confirm('Do you want to remove all items?')) {
+        $.ajax({
+            url: '/cart/all',
+            type: 'DELETE',
+            success: function() {
+                $('#cart-badge').html(0);
+                $('#cart-body').html(`<div class="alert alert-info text-center"> Your cart is empty!</div>`);
+            }
+        });
+    }
 }
--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -19,5 +19,25 @@ router.post('/', (req, res, next) => {
         })
         .catch(error => next(error));
 })
+router.put('/', (req, res) => {
+    var productId = req.body.id;
+    var quantity = parseInt(req.body.quantity);
+    var cartItem = req.session.cart.update(productId, quantity);
+    res.json(cartItem);
+});
+
+router.delete('/', (req, res) => {
+    var productId = req.body.id;
+    req.session.cart.remove(productId);
+    res.json({
+        totalQuantity: req.session.cart.totalQuantity,
+        totalPrice: req.session.cart.totalPrice
+    });
+});
+router.delete('/all', (req, res) => {
+    req.session.cart.empty();
+    res.sendStatus(204);
+    res.end();
+});
 
 module.exports = router;
