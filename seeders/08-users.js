5b59439393b22e55b5a8d2318d0ba49bf38d9ff5
Nguyen Quoc Khanh <khanhng.32@gmail.com>
2020-12-13 14:23:02 +0700

    update db

--- seeders/08-users.js
+++ seeders/08-users.js
@@ -3,10 +3,11 @@
 module.exports = {
     up: async(queryInterface, Sequelize) => {
         let data = [
-            { username: "maioanh_cute@lazy.com", password: "1", fullname: "Nguyen Ngoc Mai Oanh", avatarPath: '../img/user/maioanhcute.jpg', isAdmin: false },
-            { username: "khanh_cute@lazy.com", password: "2", fullname: "Nguyen Quoc Khanh", avatarPath: '../img/user/hellokatun.jpg', isAdmin: false },
-            { username: "thinhtran_cute@lazy.com", password: "3", fullname: "Tran Quoc Thinh", avatarPath: '../img/user/thinhtran.jpg', isAdmin: false },
-            { username: "coloan_cute@notlazy.com", password: "4", fullname: "Cô Loan", avatarPath: '../img/user/coloan.jpg', isAdmin: false },
+            { username: "maioanh_cute@lazy.com", password: "$2a$10$T1BNcd9fiOA6mhHaWXBNrO8FhtnKGndMqznlcLsjPkYwGtFbJCujK", fullname: "Nguyen Ngoc Mai Oanh", avatarPath: '../img/user/maioanhcute.jpg', isAdmin: false },
+            { username: "khanh_cute@lazy.com", password: "$2a$10$T1BNcd9fiOA6mhHaWXBNrO8FhtnKGndMqznlcLsjPkYwGtFbJCujK", fullname: "Nguyen Quoc Khanh", avatarPath: '../img/user/hellokatun.jpg', isAdmin: false },
+            { username: "thinhtran_cute@lazy.com", password: "$2a$10$T1BNcd9fiOA6mhHaWXBNrO8FhtnKGndMqznlcLsjPkYwGtFbJCujK", fullname: "Tran Quoc Thinh", avatarPath: '../img/user/thinhtran.jpg', isAdmin: false },
+            { username: "coloan_cute@notlazy.com", password: "$2a$10$T1BNcd9fiOA6mhHaWXBNrO8FhtnKGndMqznlcLsjPkYwGtFbJCujK", fullname: "Cô Loan", avatarPath: '../img/user/coloan.jpg', isAdmin: false },
+            { username: "richkid@lazy.com", password: "$2a$10$T1BNcd9fiOA6mhHaWXBNrO8FhtnKGndMqznlcLsjPkYwGtFbJCujK", fullname: "Nguyen Rich Kid", avatarPath: '../img/user/hellokatun.jpg', isAdmin: false }
         ];
         data.map(item => {
             item.createdAt = Sequelize.literal('NOW()');
